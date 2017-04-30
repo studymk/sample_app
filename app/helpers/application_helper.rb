01343f5f28a2bbc300b80d12af2a3584864b5707
mari kashimura <wakyu_j_archery_7@yahoo.co.jp>
2017-04-30 05:59:49 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページ固有のタイトル有無でタイトルの文字列を調整して返す。
+  def full_title(page_title)
+    base_title = "Tutorial"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
