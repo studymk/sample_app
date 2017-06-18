b82cc71c867b76c6c987ceaa707ab8812119644a
mari kashimura <wakyu_j_archery_7@yahoo.co.jp>
2017-06-18 01:06:27 +0000

    add image upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,10 +3,16 @@ class Micropost < ActiveRecord::Base
     default_scope -> { order('created_at DESC') }
     validates :content, presence: true, length: { maximum: 140 }
     validates :user_id, presence: true
+    
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
                          WHERE follower_id = :user_id"
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
           user_id: user.id)
   end
+  # ファイル用の属性を追加するhas_attached_fileメソッド
+  has_attached_file :image, styles: { medium: "200x150>", thumb: "50x50>" }
+
+  #  画像の拡張子を限定するためのvalidatorを定義
+  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png image/gif)
 end
